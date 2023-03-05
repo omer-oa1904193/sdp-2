81058dfc6952d1dd22f6af3d15f8ae2e07b73ce1
omer-oa1904193@qu.edu.qa <oa1904193@qu.edu.qa>
2023-03-05 13:47:20 +0300

    adds config to convert css modules to camelCase

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,21 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    webpack: (config) => {
+        //from https://github.com/vercel/next.js/discussions/11267?sort=new#discussioncomment-2352225
+        // camel-case style names from css modules
+        config.module.rules
+            .find(({oneOf}) => !!oneOf).oneOf
+            .filter(({use}) => JSON.stringify(use)?.includes("css-loader"))
+            .reduce((acc, {use}) => acc.concat(use), [])
+            .forEach(({options}) => {
+                if (options.modules) {
+                    options.modules.exportLocalsConvention = "camelCase";
+                }
+            });
 
-module.exports = nextConfig
+        return config;
+    },
+};
+
+module.exports = nextConfig;
