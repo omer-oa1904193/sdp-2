8081d3813e69b75eeee9473eb5fe2a871607256d
omer-oa1904193 <oa1904193@qu.edu.qa>
2023-03-24 13:13:02 +0300

    Creates migration for initial schema

--- backend/src/models/mikro-orm.config.ts
+++ backend/src/models/mikro-orm.config.ts
@@ -1,10 +1,12 @@
 import * as dotenv from "dotenv";
 import {LoadStrategy, Options} from "@mikro-orm/core";
+import {PostgreSqlDriver} from "@mikro-orm/postgresql";
 
 dotenv.config();
 
 const DEV_OPTIONS: Options = {
     type: "postgresql",
+    driver: PostgreSqlDriver,
     clientUrl: process.env.DATABASE_URL,
     entities: ["src/models/entities"],
     entitiesTs: ['src/models/entities'],
@@ -18,3 +20,4 @@ const DEV_OPTIONS: Options = {
 };
 
 export const mikroOrmConfig = DEV_OPTIONS;
+export default DEV_OPTIONS;
