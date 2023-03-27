a4ca4316f1ab87ff886f6476429a35f52a548d31
taimoor-th1904453 <77613021+taimoor-th1904453@users.noreply.github.com>
2023-03-27 10:41:15 +0300

    small changes in dashboard

--- frontend-js/src/pages/dashboard/dashboard.jsx
+++ frontend-js/src/pages/dashboard/dashboard.jsx
@@ -2,46 +2,68 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import NavBarAuth from '../../components/nav-bars/nav-bar-auth'
+import styles from "./DashboardPage.module.css";
+
 const Dashboard = () => {
     return (
         <>
-        <NavBarAuth></NavBarAuth>
-       <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'stretch' }}>
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexWrap: 'wrap',
-                    width: '75%',
-                    height: '100vh',
-                    flexDirection: 'column',
-                    '& > :not(style)': {
-                        m: 1,
-                        flex: '1 1 auto',
-                        minWidth: 0
-                    },
-                }}
-            >
-                <Paper elevation={3} />
-                <Paper elevation={3} />
+               {/* <div className={styles["dashboard-page"]}></div> */}
+       
+               <Box sx={{ position: 'relative', backgroundColor: '#EFEFEF' }}>
+                <NavBarAuth />
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'stretch',
+                        height: 'calc(100vh - 64px)',
+                        top: '64px'
+                    }}>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexWrap: 'wrap',
+                            width: '75%',
+                            height: '100%',
+                            flexDirection: 'column',
+                            '& > :not(style)': {
+                                mt: 3,
+                                mr: 0,
+                                mb: 1,
+                                ml: 3,
+                                flex: '1 1 auto',
+                                minWidth: 0
+                            }
+                        }}
+                    >
+                         <Paper elevation={0}  sx={{ borderRadius: '10px' }}>
+
+                         </Paper>
+                         <Paper elevation={0}  sx={{ borderRadius: '10px' }} />
+                    </Box>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexWrap: 'wrap',
+                            width: '25%',
+                            height: '50%',
+                            flexDirection: 'column',
+                            '& > :not(style)': {
+                                mt: 3,
+                                mr: 3,
+                                mb: 1,
+                                ml: 3,
+                                flex: '1 1 auto',
+                                minWidth: 0,
+                                height: '100%',
+                            }
+                        }}
+                    >
+                        <Paper elevation={0} sx={{ borderRadius: '10px' }} />
+                    </Box>
+                </Box>
             </Box>
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexWrap: 'wrap',
-                    width: '25%',
-                    height: '100vh',
-                    flexDirection: 'column',
-                    '& > :not(style)': {
-                        m: 1,
-                        flex: '1 1 auto',
-                        minWidth: 0,
-                        height: '100%',
-                    },
-                }}
-            >
-                <Paper elevation={3} />
-            </Box>
-            </Box>
+            
         </>
     );
 }
