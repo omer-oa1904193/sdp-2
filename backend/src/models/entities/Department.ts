177f15e749593f47cbc52b03e0cbe402735ec766
omer-oa1904193 <oa1904193@qu.edu.qa>
2023-04-02 03:20:15 +0300

    adds courses data

--- backend/src/models/entities/Department.ts
+++ backend/src/models/entities/Department.ts
@@ -1,17 +1,20 @@
+import type {Rel} from "@mikro-orm/core";
 import {Collection, Entity, ManyToOne, OneToMany, Property, types} from "@mikro-orm/core";
 import {CustomBaseEntity} from "./CustomBaseEntity.js";
 import {College} from "./College.js";
 import {Course} from "./Course.js";
 import {Program} from "./Program.js";
-import type {Rel} from "@mikro-orm/core";
 
 @Entity()
 export class Department extends CustomBaseEntity {
     @Property({type: types.string})
     name!: string;
 
-    @ManyToOne({entity: () => College, inversedBy: (c: College) => c.departments})
-    college!: Rel<College>;
+    @Property({type: types.string})
+    code!: string;
+
+    @ManyToOne({entity: () => College, inversedBy: (c: College) => c.departments, nullable: true})
+    college?: Rel<College>;
 
     @OneToMany({entity: () => Course, mappedBy: (c: Course) => c.department})
     courses: Collection<Course> = new Collection<Course>(this);
