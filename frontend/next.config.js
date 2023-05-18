29f542385b840ed409cfd66eab1d70b2c94864ea
omer-oa1904193 <oa1904193@qu.edu.qa>
2023-05-18 04:47:21 +0300

    Fixed style issue

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,25 +1,23 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    //from https://github.com/vercel/next.js/discussions/11267?sort=new#discussioncomment-2352225
-    // camel-case style names from css modules
-    config.module.rules
-        .find(({oneOf}) => !!oneOf).oneOf
-        .filter(({use}) => JSON.stringify(use)?.includes("css-loader"))
-        .reduce((acc, {use}) => acc.concat(use), [])
-        .forEach(({options}) => {
-          if (options.modules) {
-            options.modules.exportLocalsConvention = "camelCase";
-          }
-        });
+    reactStrictMode: true,
+    webpack: (config) => {
+        //from https://github.com/vercel/next.js/discussions/11267?sort=new#discussioncomment-2352225
+        // camel-case style names from css modules
+        config.module.rules
+            .find(({oneOf}) => !!oneOf).oneOf
+            .filter(({use}) => JSON.stringify(use)?.includes("css-loader"))
+            .reduce((acc, {use}) => acc.concat(use), [])
+            .forEach(({options}) => {
+                if (options.modules) {
+                    options.modules.exportLocalsConvention = "camelCase";
+                }
+            });
 
-    return config;
-  },
+        return config;
+    },
+
+    transpilePackages: ["@react-sigma/core"],
 }
 
-module.exports = {
-  transpilePackages: ['@react-sigma/core'],
-  nextConfig
-};
-//module.exports = nextConfig
+module.exports = nextConfig
